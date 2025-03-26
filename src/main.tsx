64102aeefb23cedbe2a45d187a0f5bf1713ca232
Yuri Sousa <ysousa.dev@gmail.com>
2025-03-26 09:33:17 -0300

    feat: :sparkles: adiciona configuraçoes pro tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import './styles.css'
+
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <h1>Hello World</h1>
   </StrictMode>,
 )
