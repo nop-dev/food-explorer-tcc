cb82631a3dfdfded1c2dcd400c9424d540d8b021
Yuri Sousa <ysousa.dev@gmail.com>
2025-03-26 09:25:13 -0300

    feat: :building_construction: adiciona vite com ts e tailwind

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+  </StrictMode>,
+)
