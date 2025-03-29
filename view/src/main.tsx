a12605633f96a1836e772c06040491e7c8f18eee
Yuri Sousa <ysousa.dev@gmail.com>
2025-03-29 10:26:05 -0300

    feat: :sparkles: estabelece base do roteamento do app

--- view/src/main.tsx
+++ view/src/main.tsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client'
 
 import './styles.css'
 
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <h1>Hello World</h1>
-  </StrictMode>,
-)
+import { Routes } from './routes'
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+	createRoot(rootElement).render(
+		<StrictMode>
+				<Routes />
+		</StrictMode>,
+	);
+}
---
+++ view/src/routes/auth.routes.tsx
@@ -0,0 +1,9 @@
+import { Route } from "react-router-dom"; 
+
+export function AuthRoutes() {
+  return (
+      <>
+        <Route path="/" />
+      </>
+  )
+}
