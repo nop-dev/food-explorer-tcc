5725f254999a2b256167e7897231b779fa4e5b7e
Yuri Sousa <ysousa.dev@gmail.com>
2025-03-31 17:07:35 -0300

    feat: :sparkles: corrige rotas para continuar o desenvolvimento

--- view/src/routes/app.routes.tsx
+++ view/src/routes/app.routes.tsx
@@ -1,15 +1,14 @@
-import { Route, Routes } from "react-router-dom"; 
+import { Navigate, Route, Routes } from "react-router-dom"; 
 
 import { Home } from "../pages/Home";
-import { SignIn } from "../pages/SignIn";
-import { SignUp } from "../pages/SignUp";
+
 export function AppRoutes() {
   return (
       <>
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/signin" element={<SignIn />}/>
-        <Route path="/signup" element={<SignUp />}/>
+
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       </>
   )
--- view/src/routes/auth.routes.tsx
+++ view/src/routes/auth.routes.tsx
@@ -1,9 +1,15 @@
-import { Route } from "react-router-dom"; 
+import { Navigate, Route } from "react-router-dom"; 
+
+import { SignIn } from "../pages/SignIn";
+import { SignUp } from "../pages/SignUp";
 
 export function AuthRoutes() {
   return (
-      <>
-        <Route path="/" />
-      </>
-  )
+			<>
+				<Route path="/signin" element={<SignIn />} />
+				<Route path="/signup" element={<SignUp />} />
+
+				<Route path="*" element={<Navigate to="/signin" />} />
+			</>
+		);
 }
