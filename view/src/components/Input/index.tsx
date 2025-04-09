527f8369f38181b500df6da14065ce06807b3b01
Yuri Sousa <ysousa.dev@gmail.com>
2025-04-09 09:31:16 -0300

    feat: :zap: faz mais alterações no componente de input

--- view/src/components/Input/index.tsx
+++ view/src/components/Input/index.tsx
@@ -1,23 +1,24 @@
-interface InputTextProps {
+interface InputProps {
 	name: string;
 	placeholder: string;
 	type: string;
 	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 	label: string;
+	className?: string;
 }
 
-export function InputText({ name, placeholder, type, onChange, label }: InputTextProps) {
+export function Input({ name, placeholder, type, onChange, label, className }: InputProps) {
 	return (
 		<div className="flex flex-col gap-2 font-roboto w-full">
 			<label htmlFor={name} className="text-light-400">
-				{label}
+				{label}		
 			</label>
 			<input
 				type={type}
 				name={name}
 				placeholder={placeholder}
 				onChange={onChange}
-				className="bg-dark-800 rounded-lg px-3.5 py-3 text-light-500 focus:border-2 focus:border-none"
+				className="bg-dark-800 rounded-lg px-3.5 py-3 text-light-500 focus:border-2 focus:border-none ${className}"
 			/>
 		</div>
 	);
